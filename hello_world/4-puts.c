da5f61f6e61d93e306d2971177594772bfb34e8f
Natiq7855 <natabbasli@gmail.com>
2025-11-26 09:12:24 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
-#!/bin/bash
-echo '#include <stdio.h>\nint main(void){puts("Programming is like building a multilingual puzzle");retutn 0;}' > 4-puts.c && gcc 4-puts.c -o 4-puts && ./4-puts
+#include <stdio.h>
+int main(void){puts("Programming is like building a multilingual puzzle");return 0;}
