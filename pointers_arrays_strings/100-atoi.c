3d9e71d054599e5ece658120e33b790e416ec37e
Natiq7855 <natabbasli@gmail.com>
2025-12-08 08:57:30 +0000

    h

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,6 +5,8 @@
  * @s: Pointer to the string
  *
  * Return: The integer value of the string
+ *
+ * Description: Stops accumulating if the next digit would cause overflow.
  */
 int _atoi(char *s)
 {
@@ -14,19 +16,35 @@ int found_digit = 0;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
-sign *= -1;  /* Flip sign for each '-' encountered */
+{
+sign *= -1;
+}
 else if (s[i] == '+')
 {
-;  /* Ignore '+' */
+;
 }
 else if (s[i] >= '0' && s[i] <= '9')
 {
 found_digit = 1;
+
+/* Check for potential overflow before multiplying/adding */
+if (num > (2147483647 - (s[i] - '0')) / 10)
+{
+if (sign == 1)
+{
+return 2147483647; /* Clamp to INT_MAX */
+}
+else
+{
+return -2147483648; /* Clamp to INT_MIN */
+}
+}
+
 num = num * 10 + (s[i] - '0');
 }
 else if (found_digit)
 {
-break;  /* Stop at first non-digit after digits */
+break;
 }
 i++;
 }
