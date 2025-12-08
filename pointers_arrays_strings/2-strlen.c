7a40f893036b8c373223a1e82130ca9472aac21b
Natiq7855 <natabbasli@gmail.com>
2025-12-08 07:47:39 +0000

    h

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @s: Pointer to the first integer.
+ * Pointer to the second integer.
+ *
+ * Description: This function takes two integer pointers and swaps
+ * the values stored at the addresses they point to.
+ */
+
+int _strlen(char *s)
+{
+int a = 0;
+while (s[a] != '\0')
+{
+a++;
+}
+return(a)
+}
