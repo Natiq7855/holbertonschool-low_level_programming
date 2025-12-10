1839f73bd7f127e510b493784e2d4d3f57644f04
Natiq7855 <natabbasli@gmail.com>
2025-12-10 05:14:59 +0000

    SDF

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: Pointer to the first element of the array.
+ * @n: Number of elements in the array.
+ *
+ * Return: Nothing.
+ */
+
+
 void reverse_array(int *a, int n)
 {
 int i, temp;
