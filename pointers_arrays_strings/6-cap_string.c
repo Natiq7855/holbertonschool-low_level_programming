a031558f59095abe1e6fe1cb2a77cabfdd626c3d
Natiq7855 <natabbasli@gmail.com>
2025-12-10 05:38:41 +0000

    Sd

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes words after sentence-starts or at the beginning
+ * cap_string - Capitalizes all words of a string
  * @s: The string to modify
  *
  * Return: Pointer to the resulting string
@@ -9,28 +9,22 @@
 char *cap_string(char *s)
 {
     int i = 0;
-    int capitalize_next = 1; /* capitalize first word and after . or ! */
+    int capitalize_next = 1;  /* Capitalize first letter of the string and after separators */
 
     while (s[i] != '\0')
     {
-        /* Capitalize if flag is set and character is lowercase */
-        if (capitalize_next && s[i] >= 'a' && s[i] <= 'z')
-        {
-            s[i] -= 32; /* convert to uppercase */
-            capitalize_next = 0;
-        }
-        /* Check for sentence-ending punctuation */
-        else if (s[i] == '.' || s[i] == '!')
+        /* Word separators */
+        if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+            s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+            s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+            s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
         {
             capitalize_next = 1;
         }
-        /* Word separators do not affect sentence capitalization */
-        else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-                 s[i] == ',' || s[i] == ';' || s[i] == '?' ||
-                 s[i] == '"' || s[i] == '(' || s[i] == ')' ||
-                 s[i] == '{' || s[i] == '}')
+        else if (capitalize_next && s[i] >= 'a' && s[i] <= 'z')
         {
-            /* keep capitalize_next as is */
+            s[i] -= 32;  /* convert lowercase to uppercase */
+            capitalize_next = 0;
         }
         else
         {
