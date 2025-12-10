3fba48615e2984bc7c71369061e3d40702def440
Natiq7855 <natabbasli@gmail.com>
2025-12-10 08:03:46 +0000

    sdf

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 unsigned int i, j;
 
 if (!*needle)  /* If needle is empty, return haystack */
-return haystack;
+return (haystack);
 
 for (i = 0; haystack[i] != '\0'; i++)
 {
