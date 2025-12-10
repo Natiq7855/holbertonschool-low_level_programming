eb07f1b97edc1b6c3388b86afa1e5cd763f24625
Natiq7855 <natabbasli@gmail.com>
2025-12-10 07:55:40 +0000

    sdf

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -6,7 +6,6 @@ char *_strpbrk(char *s, char *accept)
 unsigned int i = 0;
 unsigned int a = 0;
 unsigned int t = 0;
-int check;
 
 while (accept[a] != '\0')
 {
@@ -14,10 +13,9 @@ a++;
 }
 while (s[i] != '\0')
 {
-check = 0;
 for (t < a; t++)
 {
-if (s[i] == accept[t])
+if (s[i] == accept[t]);
 {
 return (&s[i]);
 }
