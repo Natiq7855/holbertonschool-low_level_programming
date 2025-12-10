0a36f382fe04b938031f7f0c15e0e2af8e73923b
Natiq7855 <natabbasli@gmail.com>
2025-12-10 05:37:50 +0000

    SD

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,44 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string
- * @s: The string to be modified
+ * cap_string - Capitalizes words after sentence-starts or at the beginning
+ * @s: The string to modify
  *
  * Return: Pointer to the resulting string
  */
-
 char *cap_string(char *s)
 {
-int i = 0;
-int capitalize = 1;
-while (s[i] != '\0')
-{
-if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' ||s[i] == '!' || s[i] == '?' || s[i] == '"' ||s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
-{
-capitalize = 1;
-}
-else if (capitalize && s[i] >= 'a' && s[i] <= 'z')
-{
-capitalize = 0;
-s[i] -= 32;
-}
-else if (!capitalize && s[i] >= 'A' && s[i] <= 'Z')
-{
-s[i] += 32;
-}
-else 
-{
-capitalize = 0;
-}
-i++;
-}
-return (s);
+    int i = 0;
+    int capitalize_next = 1; /* capitalize first word and after . or ! */
+
+    while (s[i] != '\0')
+    {
+        /* Capitalize if flag is set and character is lowercase */
+        if (capitalize_next && s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] -= 32; /* convert to uppercase */
+            capitalize_next = 0;
+        }
+        /* Check for sentence-ending punctuation */
+        else if (s[i] == '.' || s[i] == '!')
+        {
+            capitalize_next = 1;
+        }
+        /* Word separators do not affect sentence capitalization */
+        else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+                 s[i] == ',' || s[i] == ';' || s[i] == '?' ||
+                 s[i] == '"' || s[i] == '(' || s[i] == ')' ||
+                 s[i] == '{' || s[i] == '}')
+        {
+            /* keep capitalize_next as is */
+        }
+        else
+        {
+            capitalize_next = 0;
+        }
+
+        i++;
+    }
+
+    return s;
 }
