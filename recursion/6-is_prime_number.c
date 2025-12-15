68c61bfd62dc26234b61667ee87a202733e5e6f2
Natiq7855 <natabbasli@gmail.com>
2025-12-15 08:17:19 +0000

    DSF

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,10 +9,10 @@
 int _is_prime(int n, int i)
 {
 	if (i >= n)
-		return 1;
+		return (1);
 	if (n % i == 0)
-		return 0;
-	return _is_prime(n, i + 1);
+		return (0);
+	return (_is_prime(n, i + 1));
 }
 
 /**
@@ -23,6 +23,6 @@ int _is_prime(int n, int i)
 int is_prime_number(int n)
 {
 	if (n < 2)
-		return 0;
-	return _is_prime(n, 2);
+		return (0);
+	return (_is_prime(n, 2));
 }
