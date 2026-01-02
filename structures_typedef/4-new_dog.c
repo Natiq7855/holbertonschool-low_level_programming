677385db211acf71efbd5cbd7bbd474b06676493
Natiq7855 <natabbasli@gmail.com>
2026-01-02 21:11:15 +0000

    :SDF

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,20 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (s[len] != '\0')
+        len++;
+    return (len);
+}
+
 
 /**
  * new_dog - creates a new dog
