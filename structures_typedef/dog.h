9dc1b0a6ad463562c203ee55e6e6fab0e2242f01
Natiq7855 <natabbasli@gmail.com>
2026-01-02 20:44:53 +0000

    SDF

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,13 +14,5 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-/**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
