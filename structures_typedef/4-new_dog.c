7668d83948cbdce930d2afa20eac0e90b97563ba
Natiq7855 <natabbasli@gmail.com>
2026-01-02 21:13:18 +0000

    SDf

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,11 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 
-/**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
+/* Helper to get string length without strlen */
 int _strlen(char *s)
 {
     int len = 0;
@@ -15,41 +11,47 @@ int _strlen(char *s)
     return (len);
 }
 
-
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to new dog (dog_t), or NULL if it fails
+ * Return: pointer to new dog (dog_t), or NULL if fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
+int len_name, len_owner, i;
+
 if (name == NULL || owner == NULL)
-return(NULL);
+return (NULL);
 
 d = malloc(sizeof(dog_t));
 if (d == NULL)
 return (NULL);
 
-d->name = malloc(strlen(name) + 1);
+
+len_name = _strlen(name);
+d->name = malloc(len_name + 1);
 if (d->name == NULL)
 {
 free(d);
 return (NULL);
 }
-strcpy(d->name, name);
-d->owner = malloc(strlen(owner) + 1);
+for (i = 0; i <= len_name; i++)
+d->name[i] = name[i];
+
+len_owner = _strlen(owner);
+d->owner = malloc(len_owner + 1);
 if (d->owner == NULL)
 {
-free(d);
 free(d->name);
+free(d);
 return (NULL);
 }
-strcpy(d->owner, owner);
+for (i = 0; i <= len_owner; i++)
+d->owner[i] = owner[i];
 
 d->age = age;
 
