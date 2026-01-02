0da579a31ccdbfe1f319b6337e7999833ba178e1
Natiq7855 <natabbasli@gmail.com>
2026-01-02 21:25:36 +0000

    DSF

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a given function
+ * @name: name to print
+ * @f: pointer to a function that takes a string and returns nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+
+f(name);
+}
