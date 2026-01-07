415e0287084a0cfa9f3b53ce7cdef70c078ca9a0
Natiq7855 <natabbasli@gmail.com>
2026-01-07 10:28:30 +0000

    SDaf

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,15 +2,18 @@
 #include <stdarg.h>
 
 /**
-*sum_them all.c - sum all integers
-* @n - count of integers
-*/
-
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: The number of parameters passed to the function.
+ * @...: A variable number of parameters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ * Otherwise - the sum of all parameters.
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
-sum = 0;
+int sum = 0;
 va_start(args, n);
 if (n == 0)
 return(0);
