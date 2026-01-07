3ee079c7b4514a8a87d8bc72938af1b078e5276a
Natiq7855 <natabbasli@gmail.com>
2026-01-07 10:51:54 +0000

    SDF

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,33 +3,45 @@
 #include <stdio.h>
 
 /**
- * print_numbers - Prints numbers, followed by a new line.
- * @separator: The string to be printed between numbers.
- * @n: The number of integers passed to the function.
- * @...: A variable number of numbers to be printed.
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between the strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
+ *
+ * Description: If separator is NULL, it is not printed.
+ * If a string is NULL, (nil) is printed instead.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list args;
-unsigned int i;
-char *str = NULL;
-va_start(args, n);
+	va_list args;
+	unsigned int i;
+	char *str;
 
-for (i = 0; i < n; i++)
-{
-printf("%s", va_arg(args, char *));
-if (str == NULL)
-printf("(nil)");
-else
-printf("%s", str);
-if (separator != NULL && i < (n - 1))
-{
-printf("%s", separator);
-}
-}
+	va_start(args, n);
 
+	for (i = 0; i < n; i++)
+	{
+		/* Grab the string once per loop */
+		str = va_arg(args, char *);
 
-printf("\n");
+		/* Check if the string exists */
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
 
-va_end(args);
+		/* Print separator only if it exists and we aren't at the last string */
+		if (separator != NULL && i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+
+	va_end(args);
 }
