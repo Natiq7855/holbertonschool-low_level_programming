4f22f6f87b6c8a4d3e85a60dbb1ce66bca4527cd
Natiq7855 <natabbasli@gmail.com>
2026-01-07 10:50:58 +0000

    SDF

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
-char *str == NULL;
+char *str = NULL;
 va_start(args, n);
 
 for (i = 0; i < n; i++)
