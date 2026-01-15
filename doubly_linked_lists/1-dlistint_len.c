bfdf3b819e91ba5153d7fed430bd496ba9f8c96f
Natiq7855 <natabbasli@gmail.com>
2026-01-15 14:43:48 +0000

    ASDF

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * dlistint_len - returns the number of elements in a doubly linked list
+ * @h: pointer to the first node of the list
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t len = 0;
+while (h != NULL)
+{
+len++;
+h = h->next;
+}
+return (len);
+}
