0ef21b7385ad7de46dc7657af2dd357928ba8438
Natiq7855 <natabbasli@gmail.com>
2026-01-15 14:52:36 +0000

    SDF

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+
+    if (head == NULL)
+    {
+        return (NULL);
+    }
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+    new->n = n;
+    new->prev = NULL;
+    new->next = *head;
+
+    if (head != NULL)
+    {
+        (*head)->prev = new;
+    }
+    return(new);
+}
