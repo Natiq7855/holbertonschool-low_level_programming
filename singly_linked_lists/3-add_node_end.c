2aa219636b11e067bb34b02c6c4963121364bf5d
Natiq7855 <natabbasli@gmail.com>
2026-01-15 14:21:09 +0000

    FDS

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,45 +13,45 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *temp;
-    unsigned int len = 0;
+list_t *new, *temp;
+unsigned int len = 0;
 
-    if (head == NULL || str == NULL)
-    {
-        return (NULL);
-    }
+if (head == NULL || str == NULL)
+{
+return (NULL);
+}
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        return (NULL);
-    }
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
 
-    new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
 
 
-    while (str[len])
-    {
-        len++;
-    }
-    new->len = len;
-    new->next = NULL;
+while (str[len])
+{
+len++;
+}
+new->len = len;
+new->next = NULL;
 
-    if (*head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
 
-	temp = *head;
-	while (temp->next != NULL)
-		temp = temp->next;
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
 
-	temp->next = new;
-    return (new);    
+temp->next = new;
+return (new);
 }
