cfa21d24df3eca9848feddcd9221818db81579ed
Natiq7855 <natabbasli@gmail.com>
2026-01-15 15:22:20 +0000

    SDf

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -5,11 +5,11 @@
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
-    while(head != NULL)
-    {
-        temp = head->next;
-        free(head);
-        head = temp
-    }
+dlistint_t *temp;
+while(head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp
+}
 }
