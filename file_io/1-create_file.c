5f8865170599f31a6b6e96292597fca6df321170
Natiq7855 <natabbasli@gmail.com>
2026-01-22 06:33:47 +0000

    SDF

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,6 +4,14 @@
 #include <sys/stat.h>
 #include "main.h"
 
+/**
+ * create_file - Creates a file with specific permissions and content.
+ * @filename: Name of the file to create.
+ * @text_content: NULL terminated string to write.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 int i = 0;
@@ -11,7 +19,7 @@ ssize_t n_wrote;
 int fd;
 if (filename == NULL)
 {
-return(-1);
+return (-1);
 }
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
@@ -33,5 +41,5 @@ return (-1);
 }
 
 close(fd);
-return(1);
+return (1);
 }
