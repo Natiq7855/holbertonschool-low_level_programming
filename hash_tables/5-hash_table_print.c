30729c41ffa47ee311238994c2cece7e03ea0ca4
Natiq7855 <natabbasli@gmail.com>
2026-01-24 10:57:33 +0000

    SDF

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table.
+ *
+ * Return: void.
+ */
+
 void hash_table_print(const hash_table_t *ht)
 {
 hash_node_t *node;
@@ -28,5 +35,5 @@ flag = 1;
 node = node->next;
 }
 }
-printf("}")
+printf("}");
 }
