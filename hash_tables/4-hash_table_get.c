9a2f4d147da4b50314b9d7354b99f6b27acf11df
Natiq7855 <natabbasli@gmail.com>
2026-01-24 10:45:17 +0000

    SDF

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -2,6 +2,15 @@
 #include <string.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: A pointer to the hash table struct.
+ * @key: The key you are looking for.
+ *
+ * Return: The value associated with the element,
+ * or NULL if key couldn’t be found.
+ */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 hash_node_t *node;
