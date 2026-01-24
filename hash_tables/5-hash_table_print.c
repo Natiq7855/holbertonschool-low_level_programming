df3202e9e294b6e56134f60e9256b079d0258c00
Natiq7855 <natabbasli@gmail.com>
2026-01-24 10:59:29 +0000

    SDF

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,31 +9,31 @@
  *
  * Return: void.
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
-hash_node_t *node;
-unsigned long int i;
-int flag = 0;
+	hash_node_t *node;
+	unsigned long int i;
+	int flag = 0;
 
-if (ht == NULL)
-{
-return;
-}
-printf("{");
-for (i = 0; i < ht->size; i++)
-{
-node = ht->array[i];
-while (node != NULL)
-{
-if (flag == 1)
-{
-printf(", ");
-}
-printf("'%s' : '%s'", node->key, node->value);
-flag = 1;
-node = node->next;
-}
-}
-printf("}\n");
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (flag == 1)
+			{
+				printf(", ");
+			}
+			/* Added single quotes and colon to match desired format */
+			printf("'%s': '%s'", node->key, node->value);
+			flag = 1;
+			node = node->next;
+		}
+	}
+	/* Added newline at the end */
+	printf("}\n");
 }
