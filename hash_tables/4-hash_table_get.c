f4a56a9b841eb1e60cf4df2d6a5ea8ed67eb1ae2
Natiq7855 <natabbasli@gmail.com>
2026-01-24 10:41:47 +0000

    SDF

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+hash_node_t *node;
+unsigned long int index;
+
+if (ht == NULL || key == NULL || *key == '\0')
+{
+return (NULL);
+}
+
+index = key_index((const unsigned char *)key, ht->size);
+
+node = ht->array[index];
+
+while(node != NULL)
+{
+if (strdup(node->key, key) != 0)
+{
+return (node->value);
+}
+node = node->next;
+}
+return(NULL);
+}
